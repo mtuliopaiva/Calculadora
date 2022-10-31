5f4fa2f5b656d8ed2e6c47b969f4fbe1a5552ec5
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-10-31 14:42:35 -0300

    Calculadora funcionando - Trabalhando na notação cientifica

--- src/components/Calculadora/Calculadora.js
+++ src/components/Calculadora/Calculadora.js
@@ -3,67 +3,63 @@ import Display from '../Display/Display'
 import Keyboard from '../Keyboard/Keyboard'
 import './Calculadora.css'
 import Header from '../Header/Header'
-import { useState} from 'react'
+import { useState } from 'react'
 
 const Calculadora = () => {
 
-    const [inputDisplay,setInputDisplay] = useState(0);
-    const [oldValue,setOldValue] = useState('');
-    const [operator,setOperator] = useState('');
+    const [inputDisplay, setInputDisplay] = useState(0);
+    const [oldValue, setOldValue] = useState('');
+    const [operator, setOperator] = useState('');
 
-    /*d é o dígito*/ 
+    /*d é o dígito*/
     const insereNum = (d) => {
-        if(inputDisplay == 0 ){
+        if (inputDisplay == 0) {
             setInputDisplay(d);
         }
-        else{
+        else {
             setInputDisplay(inputDisplay + d);
-        }
-        
-        
-        // console.log(valor);
-        
-        if(d =='+' ||d =='-' ||d =='/' ||d =='*' ) {
-            console.log("Operações");
-            setOperator(d);
-            setOldValue(inputDisplay);
-            setInputDisplay(0);
-        }
-        
-        if(d ==='='){
-            console.log(oldValue);
-            console.log(operator);
-            console.log(inputDisplay);
-            let r = oldValue + operator + inputDisplay;
-            let result = eval(r);
-            setInputDisplay(result);
+            if(inputDisplay.length>9){
+                console.log(parseFloat(inputDisplay));
+                let a = parseFloat(inputDisplay).toPrecision(5);
+                setInputDisplay(a);
+            }
+
+            if (d == '+' || d == '-' || d == '/' || d == '*') {
+                console.log("Operações");
+                setOperator(d);
+                setOldValue(inputDisplay);
+                setInputDisplay(0);
+
+            }
+            if (d === '=') {
+                console.log(oldValue);
+                console.log(operator);
+                console.log(inputDisplay);
+                let r = oldValue + operator + inputDisplay;
+                let result = eval(r);
+                setInputDisplay(result);
+            }
+            if (d === 'AC') {
+                setInputDisplay(0);
+            }
+            if (d === '+/-') {
+                const r = parseFloat(inputDisplay) * -1;
+                setInputDisplay(r);
+            }
+            if (d === '%') {
+                const r = parseFloat(inputDisplay) / 100;
+                setInputDisplay(r);
+            }
 
         }
-        //     setOperado(true);
-        //     console.log(operado);
-        //         let r = eval(inputDisplay);
-        //         setInputDisplay(r);
-        //         console.log("Igual");
-        // }
-    if(d ==='AC'){
-        setInputDisplay(0);
-    }
-    if(d === '+/-'){
-        const r = parseFloat(inputDisplay)*-1;
-        setInputDisplay(r);
-    }
-    if(d === '%'){
-        const r = parseFloat(inputDisplay)/100;
-        setInputDisplay(r);
-    }
 
     }
 
-    return(
+    return (
         <div className='container'>
             <Header />
-            <Display  disp={inputDisplay}/>
-            <Keyboard insertDisplay={insereNum}/>
+            <Display disp={inputDisplay} />
+            <Keyboard insertDisplay={insereNum} />
         </div>
     )
 }
